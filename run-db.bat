29b2edc8ab1fd32fca3afc9beb6145154d373823
subway90 <nguyenhieu3105@gmail.com>
2024-10-21 22:58:18 +0700

    update run-db batch

--- run-db.bat
+++ run-db.bat
@@ -3,3 +3,6 @@ php artisan migrate:refresh --seed
 php artisan db:seed --class=KhuVucSeeder
 php artisan db:seed --class=ToaNhaSeeder
 php artisan db:seed --class=PhongSeeder
+php artisan db:seed --class=HopDongSeeder
+php artisan db:seed --class=MaUuDaiSeeder
+php artisan db:seed --class=ThanhToanSeeder
