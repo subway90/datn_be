996adfd0b03b20e298b52dc0574f2e610ca49e94
subway90 <nguyenhieu3105@gmail.com>
2024-10-26 18:03:51 +0700

    update Database & Seeder: change value fieild "avatar" to nullable

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Nguyễn Minh Hiếu',
             'email' => 'nguyenhieu3105@gmail.com',
             'password' => Hash::make('12345678'),
-            'avatar' => 'user1.jpg',
             'born' => '2024-05-31',
             'phone' => '0965279041',
             'role' => 0, //admin
@@ -30,7 +29,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Tester 01',
             'email' => 'test01@gmail.com',
             'password' => Hash::make('12345678'),
-            'avatar' => 'test01.jpg',
             'born' => '2024-07-09',
             'phone' => '0332665391',
         ]);
@@ -38,7 +36,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Tester 02',
             'email' => 'test02@gmail.com',
             'password' => Hash::make('12345678'),
-            'avatar' => 'test02.jpg',
             'born' => '2024-05-31',
             'phone' => '0888442652',
         ]);
@@ -46,7 +43,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Tester 03',
             'email' => 'test03@gmail.com',
             'password' => Hash::make('12345678'),
-            'avatar' => 'test03.jpg',
             'born' => '2024-01-17',
             'phone' => '0969455222',
         ]);
