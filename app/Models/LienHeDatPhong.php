12d77625a8e44ed2629f21c69ebce2b4e5a2bb3a
subway90 <nguyenhieu3105@gmail.com>
2024-10-27 09:49:46 +0700

    update Database LienHeDatPhong: add SoftDelete

--- app/Models/LienHeDatPhong.php
+++ app/Models/LienHeDatPhong.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class LienHeDatPhong extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $table = 'lien-he-dat-phong'; // Tên bảng nếu không theo quy tắc Laravel
 
--- database/migrations/2024_10_25_184054_create_lien-he-dat-phong_table.php
+++ database/migrations/2024_10_25_184054_create_lien-he-dat-phong_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->timestamps();
             $table->foreign('phong_id')->references('id')->on('phong');
             $table->foreign('tai_khoan_id')->references('id')->on('users');
+            $table->softDeletes();
         });
     }
 
