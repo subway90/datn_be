1bb01be022d92d179b1ca613797a89b0adc1103e
subway90 <nguyenhieu3105@gmail.com>
2024-10-27 14:58:17 +0700

    update API GET Dashboard: delete total_view, add total_contract

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\ToaNha;
 use App\Models\User;
+use App\Models\HopDong;
 use App\Models\LienHeDatPhong;
 use Illuminate\Http\Request;
 
@@ -10,12 +10,12 @@ class DashBoardController extends Controller
 {
     public function total()
     {
-        $total_view = ToaNha::sum('luot_xem');
+        $total_contract = HopDong::sum('luot_xem');
         $total_user = User::count();
         $total_contact = LienHeDatPhong::count();
         
         return response()->json([
-           'total_view' => $total_view,
+           'total_contract' => $total_contract,
            'total_user' => $total_user,
            'total_contact_room' => $total_contact,
         ], 200);
