63c20337fbbe7b8889cbbc4657761c557ca96f70
subway90 <nguyenhieu3105@gmail.com>
2024-10-28 17:38:11 +0700

    update Seeder/KhuVuc: change value key "image"

--- database/seeders/KhuVucSeeder.php
+++ database/seeders/KhuVucSeeder.php
@@ -12,48 +12,48 @@ class KhuVucSeeder extends Seeder
         KhuVuc::create([
             'ten' => 'Quận 12',
             'slug' => 'quan-12',
-            'image' => 'http://localhost:8000/storage/area/quan12.jpg',
+            'image' => 'area/quan12.jpg',
             'noi_bat'=> 1,
         ]);
 
         KhuVuc::create([
             'ten' => 'Quận Gò Vấp',
             'slug' => 'quan-go-vap',
-            'image' => 'http://localhost:8000/storage/area/quangovap.jpg',
+            'image' => 'area/quangovap.jpg',
             'noi_bat'=> 1,
         ]);
 
         KhuVuc::create([
             'ten' => 'Quận Hóc Môn',
             'slug' => 'quan-hoc-mon',
-            'image' => 'http://localhost:8000/storage/area/quanhocmon.jpg',
+            'image' => 'area/quanhocmon.jpg',
             'noi_bat'=> 1,
         ]);
 
         KhuVuc::create([
             'ten' => 'Quận Tân Bình',
             'slug' => 'quan-tan-binh',
-            'image' => 'http://localhost:8000/storage/area/quantanbinh.jpg',
+            'image' => 'area/quantanbinh.jpg',
             'noi_bat'=> 1,
         ]);
 
         KhuVuc::create([
             'ten' => 'Quận Bình Tân',
             'slug' => 'quan-binh-tan',
-            'image' => 'http://localhost:8000/storage/area/quanbinhtan.jpg',
+            'image' => 'area/quanbinhtan.jpg',
         ]);
 
         KhuVuc::create([
             'ten' => 'Quận Phú Nhuận',
             'slug' => 'quan-phu-nhuan',
-            'image' => 'http://localhost:8000/storage/area/quanphunhuan.jpg',
+            'image' => 'area/quanphunhuan.jpg',
             'noi_bat'=> 1,
         ]);
 
         KhuVuc::create([
             'ten' => 'Quận Tân Phú',
             'slug' => 'quan-tan-phu',
-            'image' => 'http://localhost:8000/storage/area/quantanphu.jpg',
+            'image' => 'area/quantanphu.jpg',
             'noi_bat'=> 1,
         ]);
     }
