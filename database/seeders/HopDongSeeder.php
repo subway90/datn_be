fe1b77aaa6f6f6610695797be900b374730adb2e
subway90 <nguyenhieu3105@gmail.com>
2024-11-14 12:42:12 +0700

    update Database HopDong : change type dateTimes to Date

--- database/seeders/HopDongSeeder.php
+++ database/seeders/HopDongSeeder.php
@@ -13,7 +13,7 @@ class HopDongSeeder extends Seeder
             'phong_id' => 1,
             'tai_khoan_id' => 3,
             'ngay_bat_dau' => '2023-01-01',
-            'ngay_ket_thuc' => '2024-06-31',
+            'ngay_ket_thuc' => '2024-06-01',
             'gia_thue' => 3250000,
         ]);
 
