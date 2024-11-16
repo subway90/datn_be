948c20458d3670939552881505070dd63293650e
subway90 <nguyenhieu3105@gmail.com>
2024-11-16 09:03:22 +0700

    update Controller YeuThich : delete function not use

--- app/Http/Controllers/YeuThichController.php
+++ app/Http/Controllers/YeuThichController.php
@@ -61,44 +61,4 @@ class YeuThichController extends Controller
             return response()->json(['message' => 'Đã thêm vào danh sách yêu thích.'], 201);
         }
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
