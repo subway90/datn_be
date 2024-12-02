f528f4ef9911c1e5fa3dc40df5498fded0c55f13
subway90 <nguyenhieu3105@gmail.com>
2024-12-02 22:03:37 +0700

    add view 404

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('emails.success_email_register');
+    // return view('emails.success_email_register');
+    return view('404');
+
 });
 
 Route::get('/pay_result', function () {
