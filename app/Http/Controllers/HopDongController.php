0c3558bc7901aae43af3f91427b2b8e2093edb47
nguyennhattan2328 <guyennhattan2328@gmail.com>
2024-12-15 17:59:22 +0700

    Add danh sách hết hạn

--- app/Http/Controllers/HopDongController.php
+++ app/Http/Controllers/HopDongController.php
@@ -16,16 +16,16 @@ class HopDongController extends Controller
 {
     public function index()
     {
-        $list = HopDong::orderBy('id','DESC')->get();
+        $list = HopDong::where('ngay_ket_thuc', '>=', now())->orderBy('id', 'DESC')->get();
         $result = $list->map(function ($row) {
-            $room = Phong::withTrashed()->with('toaNha')->where('id',$row->phong_id)->get(['toa_nha_id','ten_phong','hinh_anh'])->first();
-            $user = User::withTrashed()->where('id',$row->tai_khoan_id)->get(['name','avatar'])->first();
-            $building = ToaNha::withTrashed()->where('id',$room->toa_nha_id)->get(['ten'])->first();
+            $room = Phong::withTrashed()->with('toaNha')->where('id', $row->phong_id)->get(['toa_nha_id', 'ten_phong', 'hinh_anh'])->first();
+            $user = User::withTrashed()->where('id', $row->tai_khoan_id)->get(['name', 'avatar'])->first();
+            $building = ToaNha::withTrashed()->where('id', $room->toa_nha_id)->get(['ten'])->first();
             return [
                 'id' => $row->id,
                 'id_room' => $row->phong_id,
                 'name_room' => $room->ten_phong,
-                'name_building' =>$building->ten,
+                'name_building' => $building->ten,
                 'image_room' => Str::before($room->hinh_anh, ';'),
                 'id_user' => $row->tai_khoan_id,
                 'name_user' => $user->name,
@@ -35,12 +35,44 @@ class HopDongController extends Controller
                 'file_hop_dong' => $row->file_hop_dong,
                 'date_start' => $row->ngay_bat_dau,
                 'date_end' => $row->ngay_ket_thuc,
-                'status' => $row->ngay_ket_thuc < $this->date_now ? 'Hết hạn' : 'Đang sử dụng',
+                'status' => 'Đang sử dụng',
             ];
         });
+    
         return response()->json($result, 200);
     }
+    public function het_han(){
+        
+    $list = HopDong::where('ngay_ket_thuc', '<', now())
+        ->orderBy('id', 'DESC')
+        ->get();
 
+    $result = $list->map(function ($row) {
+        $room = Phong::withTrashed()->with('toaNha')->where('id', $row->phong_id)->get(['toa_nha_id', 'ten_phong', 'hinh_anh'])->first();
+        $user = User::withTrashed()->where('id', $row->tai_khoan_id)->get(['name', 'avatar'])->first();
+        $building = ToaNha::withTrashed()->where('id', $room->toa_nha_id)->get(['ten'])->first();
+
+        return [
+            'id' => $row->id,
+            'id_room' => $row->phong_id,
+            'name_room' => $room->ten_phong,
+            'name_building' => $building->ten,
+            'image_room' => Str::before($room->hinh_anh, ';'),
+            'id_user' => $row->tai_khoan_id,
+            'name_user' => $user->name,
+            'avatar_user' => $user->avatar ?? 'avatar/user_default.png',
+            'so_luong_xe' => $row->so_luong_xe,
+            'so_luong_nguoi' => $row->so_luong_nguoi,
+            'file_hop_dong' => $row->file_hop_dong,
+            'date_start' => $row->ngay_bat_dau,
+            'date_end' => $row->ngay_ket_thuc,
+            'status' => 'Hết hạn',
+        ];
+    });
+
+    return response()->json($result, 200);
+}
+   
     public function detail($id)
     {
         $row = HopDong::find($id);
--- routes/api.php
+++ routes/api.php
@@ -152,8 +152,11 @@ Route::middleware(['Admin'])->group(function () {
 
     Route::prefix('hop-dong')->group(function () {
 
-        # Lấy danh sách
-        Route::get('/all', [HopDongController::class, 'index']);
+        # Lấy danh sách đang hoạt động 
+        Route::get('/hoat_dong', [HopDongController::class, 'index']);
+
+         # Lấy danh sách hết hạn
+         Route::get('/het_han', [HopDongController::class, 'het_han']);
 
         # Lấy danh sách đã xóa
         Route::get('/list_delete', [HopDongController::class, 'list_delete']);
