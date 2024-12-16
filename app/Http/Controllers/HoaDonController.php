681f072a082548b98e501b2cf4875a79c6223836
subway90 <nguyenhieu3105@gmail.com>
2024-12-16 14:42:30 +0700

    update API hoa-don/add : validate, render data Phong & HopDong

--- app/Http/Controllers/HoaDonController.php
+++ app/Http/Controllers/HoaDonController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HopDong;
 use Illuminate\Http\Request;
 use App\Models\HoaDon;
+use App\Models\Phong;
 use Illuminate\Support\Facades\Validator;
 use Ramsey\Uuid\Uuid;
 
@@ -13,37 +15,16 @@ class HoaDonController extends Controller
         // Xác thực dữ liệu đầu vào
         $validate = Validator::make($request->all(),[
             'id_hop_dong' => 'required|exists:hop_dong,id', // Kiểm tra hợp đồng tồn tại
-            'tien_thue' => 'required|numeric',
-            'tien_dien' => 'required|numeric',
             'so_ki_dien' => 'required|numeric',
-            'tien_nuoc' => 'required|numeric',
             'so_khoi_nuoc' => 'required|numeric',
-            'tien_xe' => 'required|numeric',
-            'so_luong_xe' => 'required|numeric',
-            'tien_dich_vu' => 'required|numeric',
-            'so_luong_nguoi' => 'required|numeric',
             'noi_dung' => 'nullable|string',
         ],[
             'id_hop_dong.required' => 'Chưa nhập ID Hợp đồng',
             'id_hop_dong.exists' => 'ID hợp đồng này không tồn tại',
-            'tien_thue.required' => 'Chưa nhập tiền thuê',
-            'tien_thue.numeric' => 'Tiền thuê phải là số',
-            'tien_dien.required' => 'Chưa nhập tiền điện',
-            'tien_dien.numeric' => 'Tiền điện phải là số',
             'so_ki_dien.required' => 'Chưa nhập Số kí điện',
             'so_ki_dien.numeric' => 'Số kí điện phải là số',
-            'tien_nuoc.required' => 'Chưa nhập tiền nước',
-            'tien_nuoc.numeric' => 'Tiền nước phải là số',
             'so_khoi_nuoc.required' => 'Chưa nhập Số khối nước',
             'so_khoi_nuoc.numeric' => 'Số khối nước phải là số',
-            'tien_xe.required' => 'Chưa nhập tiền xe',
-            'tien_xe.numeric' => 'Tiền xe phải là số',
-            'so_luong_xe.required' => 'Chưa nhập Số lượng xe',
-            'so_luong_xe.numeric' => 'Số lượng xe phải là số',
-            'tien_dich_vu.required' => 'Chưa nhập tiền dịch vụ',
-            'tien_dich_vu.numeric' => 'Tiền dịch vụ phải là số',
-            'so_luong_nguoi.required' => 'Chưa nhập Số lượng người',
-            'so_luong_nguoi.numeric' => 'Số lượng người phải là số',
             'noi_dung.string' => 'Nội dung phải là chuỗi',
         ]);
         if($validate->fails()) return response()->json(['message'=>$validate->errors()->all()],400);
@@ -55,19 +36,23 @@ class HoaDonController extends Controller
             // if($this->date_now->diffInDays($get_contract->ngay_bat_dau) > 30)
         // }
 
+        // Lấy thông tin phòng từ hợp đồng
+        $get_contract = HopDong::select('phong_id','so_luong_xe','so_luong_nguoi')->where('id',$request->id_hop_dong)->first();
+        $get_room = Phong::find($get_contract->phong_id);
+
         // Tạo hóa đơn mới
         $hoaDon = HoaDon::create([
             'token' => Uuid::uuid4()->toString(),
             'hop_dong_id' => $request->id_hop_dong,
-            'tien_thue' => $request->tien_thue,
-            'tien_dien' => $request->tien_dien,
+            'tien_thue' => $get_room->gia_thue,
+            'tien_dien' => $get_room->don_gia_dien,
+            'tien_nuoc' => $get_room->don_gia_nuoc,
+            'tien_xe' => $get_room->tien_xe_may,
+            'tien_dich_vu' => $get_room->phi_dich_vu,
+            'so_luong_xe' => $get_contract->so_luong_xe,
+            'so_luong_nguoi' => $get_contract->so_luong_nguoi,
             'so_ki_dien' => $request->so_ki_dien,
-            'tien_nuoc' => $request->tien_nuoc,
             'so_khoi_nuoc' => $request->so_khoi_nuoc,
-            'tien_xe' => $request->tien_xe,
-            'so_luong_xe' => $request->so_luong_xe,
-            'tien_dich_vu' => $request->tien_dich_vu,
-            'so_luong_nguoi' => $request->so_luong_nguoi,
             'noi_dung' => $request->noi_dung,
         ]);
 
